087b4d36fadd1e115906dfb23043938f64e948ce
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2025-01-05 16:48:06 +0530

    Main Updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,79 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.controller.DepartmentController;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("1. Department Operations");
+            System.out.println("2. Employee Operations");
+            System.out.println("3. Quit");
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice){
+                case 1 -> departmentMenu();
+                case 2 -> empolyeeMenu();
+                case 3 -> {
+                    System.out.println("Exiting program...");
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid choice");
+            }
+
+        }
+    }
+
+    public static void departmentMenu(){
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("____Department Operations____");
+            System.out.println("1. Create Department");
+            System.out.println("2. Get All Departments");
+            System.out.println("3. Update Department");
+            System.out.println("4. Delete Department");
+            System.out.println("0. Back to Mane Menu");
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice){
+                case 1-> DepartmentController.addDepartment();
+                case 2-> DepartmentController.getAllDepartments();
+                case 3 -> DepartmentController.updateDepartment();
+                case 4 -> DepartmentController.deleteDepartment();
+                case 0 -> {
+                    System.out.println("Back to Mane Menu...");
+                    return;
+                }
+                default -> System.out.println("Invalid choice");
+            }
+        }
+    }
+    public static void empolyeeMenu(){
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("____Employee Operations____");
+            System.out.println("1. Create Employee");
+            System.out.println("2. Get All Employees");
+            System.out.println("3. Update Employee");
+            System.out.println("4. Delete Employee");
+            System.out.println("0. Back to Mane Menu");
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice){
+
+            }
         }
     }
 }
