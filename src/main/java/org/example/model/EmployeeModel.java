80911bb692a52af558f660769a538da892713a7e
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2025-01-08 23:40:29 +0530

    Updated

--- src/main/java/org/example/model/EmployeeModel.java
+++ src/main/java/org/example/model/EmployeeModel.java
@@ -1,6 +1,7 @@
 package org.example.model;
 
 import org.example.controller.EmployeeController;
+import org.example.entity.Department;
 import org.example.utill.GetSessionFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,11 +10,13 @@ public class EmployeeModel {
 
     SessionFactory sessionFactory = GetSessionFactory.getSessionFactory();
 
-    public void addEmployee(EmployeeController employee) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(employee);
-        session.getTransaction().commit();
+    public void saveEmployee(EmployeeController employee) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            Department department = session.createQuery("from Department where  name = :name",Department.class);
+
+        }
     }
 
     public void getEmployee(long id) {
